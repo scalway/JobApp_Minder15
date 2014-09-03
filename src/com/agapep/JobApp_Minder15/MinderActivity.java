428e841860aefcb6aa740997059799683202b9a2
slovic <slovic.r@gmail.com>
2014-09-03 18:17:54 +0200

    Added background task support (actually download mp3 file). changed time to 19:00

--- src/com/agapep/JobApp_Minder15/MinderActivity.java
+++ src/com/agapep/JobApp_Minder15/MinderActivity.java
@@ -1,9 +1,12 @@
 package com.agapep.JobApp_Minder15;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
-public class MinderActivity extends Activity {
+public class MinderActivity extends Activity implements View.OnClickListener {
     /**
      * Called when the activity is first created.
      */
@@ -11,5 +14,14 @@ public class MinderActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        findViewById(R.id.textView).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Log.d("MinderApp", "start downloading from activity");
+        Intent intent = new Intent(getBaseContext(), DoSomethingImportantService.class);
+        intent.putExtra("url", "http://www.biblia-mp3.pl/Ewangelia_Mateusza_1-7.mp3");
+        startService(intent);
     }
 }
